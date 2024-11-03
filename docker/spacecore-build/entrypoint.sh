252358a862936c2521e3830cd59c567c1cdbe231
Edwin Eefting <edwin@datux.nl>
2024-11-03 20:26:29 +0100

    also auto create admin user the first time

--- docker/spacecore-build/entrypoint.sh
+++ docker/spacecore-build/entrypoint.sh
@@ -18,7 +18,12 @@ done
 # init db?
 if ! mysqldump db users >/dev/null; then
     echo "Init db..."
-    mysql <schema.sql
+    mysql <schema.sql || exit 1
+
+    echo "Create initial admin user"
+    CRYPTED_PASS=$(mkpasswd -m sha-512 "$ADMIN_PASSWORD")
+    echo "INSERT INTO users (id, user_name, full_name, title, password, active, avatar_id) VALUES (NULL, 'admin', 'admin', 'bofh', '$CRYPTED_PASS', '1', NULL);" | mysql db
+
 fi
 
 echo "Starting app.js..."
